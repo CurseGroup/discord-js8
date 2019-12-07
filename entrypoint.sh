fedc77e4e54bc36472000b2480c4bf2639a69f83
Arnaud Lier <zeprofdecoding@gmail.com>
2019-12-07 19:53:28 +0100

    Create entrypoint.sh

---
+++ entrypoint.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+cd /home/container
+
+MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
+echo ":/home/container$ ${MODIFIED_STARTUP}"
+
+# Run the Server
+npm install
+${MODIFIED_STARTUP}
